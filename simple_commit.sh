4791591730ab3a779ad959145dab92b2daa8561f
Boris Veltman <baveltman@gmail.com>
2016-10-11 23:10:49 -0700

    $message

---
+++ simple_commit.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#simple git commit script to master
+
+branch=$1
+message=$2
+
+if [[ -z "$branch" ]]
+then
+    echo "usage simple_commit {branch} '{message}'"
+    echo "missing {branch}"
+    exit
+fi
+
+if [[ -z "$message" ]]
+then
+    echo "usage simple_commit {branch} '{message}'"
+    echo "missing {message}"
+    exit
+fi
+
+echo "## Adding all changes..."
+echo "$(git add .)"
+echo ""
+echo "## Status:"
+echo ""
+echo "$(git status)"
+echo ""
+echo "## Commiting changes with message $message"
+echo "$(git commit -m '$message')"
+echo ""
+echo "## Rebasing and pushing"
+echo "$(git pull --rebase origin $branch)"
+echo "$(git push -u origin $branch)"
+
+exit
